3da54158d219b9f1e12d02e9a05e6181da1fa983
fsubal <fsubal1102@gmail.com>
2019-11-12 10:29:13 +0900

    init jest

---
+++ src/__tests__/index.spec.tsx
@@ -0,0 +1,15 @@
+import DialogProvider, { useDialog } from ".."
+
+describe("react-async-dialog/index", () => {
+  describe("DialogProvider", () => {
+    it.skip("renders", () => {})
+
+    it.skip("mounts modal to non-document.body element", () => {})
+
+    it.skip("allow override default layout", () => {})
+  })
+
+  describe("useDialog", () => {
+    it.skip("subscribes parent context correctly", () => {})
+  })
+})
